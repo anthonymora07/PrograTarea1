8fc096a8b9831450e86a3413794bce1e9763b2fa
Anthony Mora <anthony.mora.valverde@est.una.ac.cr>
2021-10-03 01:04:01 -0600

    1.0.2

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    class DatabaseService implements Conexion {
+
+        @Override
+        public Dato getDatos() {}
+
+        @Override
+        public void setDatos() {}
+    }
+
+    class APIService implements Conexion{
+
+        @Override
+        public Dato getDatos() {}
+
+        @Override
+        public void setDatos() {}
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+}
